538feb6fb3c479fc48fde6740992d55394837f57
Petar Tsankov <petar.tsankov@gmail.com>
2015-10-26 15:04:40 +0100

    .

--- scripts/synth.py
+++ scripts/synth.py
@@ -16,34 +16,36 @@ from ctl import ctl_grammar
 import policy_synth
 from networkx.classes.function import set_node_attributes
 
+def solve_synthesis_instance(reqsStr, num_ors, num_enum, num_numeric):
+    policy_synth.setTemplateSize(num_ors, num_enum, num_numeric)            
+    reqs = []
+    for reqStr in reqsStr:
+        if not reqStr.startswith('('):
+            print reqStr
+        else:
+            reqs.append(ctl_grammar.parseRequirement(reqStr))    
+    return policy_synth.synth(graph, reqs, attrs)
+
 if __name__ == '__main__':
     
-    if len(sys.argv) != 8:
-        print 'Usage: {} <graph file> <attributes file> <resources file> <requirements> <num ors> <# enums> <# numeric>'.format(sys.argv[0])
+    if len(sys.argv) not in [5, 8]:
+        print 'Usage: {} <graph file> <attributes file> <resources file> <requirements> [<num ors> <# enums> <# numeric>]'.format(sys.argv[0])
         sys.exit(-1)
     graphFilename = sys.argv[1]
     attributesFilename = sys.argv[2]
     resourcesFilename = sys.argv[3]
     reqsFilename = sys.argv[4]
 
-    policy_synth.setTemplateSize(int(sys.argv[5]), int(sys.argv[6]), int(sys.argv[7]))    
-    
-    for filename in [graphFilename, attributesFilename, resourcesFilename, reqsFilename]:
-        if not os.path.isfile(filename):
-            print filename, 'is not a file'
-            sys.exit(-1)
-            
-#    algorithm = sys.argv[5]
-#    if algorithm not in ['boolean-policy', 'decompose', 'negative', 'policy']:
-#        print 'Unsupported algorithm. The supported ones are: decompose, negative, boolean-policy, policy'
-#        sys.exit(-1)
-            
     print 'Resource graph filename:', graphFilename
     print 'Attributes filename:', attributesFilename
     print 'Resources  filename:', resourcesFilename
     print 'Requirements filename:', reqsFilename
-    
-    
+
+    for filename in [graphFilename, attributesFilename, resourcesFilename, reqsFilename]:
+        if not os.path.isfile(filename):
+            print filename, 'is not a file'
+            sys.exit(-1)            
+
     graph = nx.read_adjlist(graphFilename, create_using = nx.DiGraph())    
     print 'Resources in the resource graph:', graph.nodes()    
         
@@ -52,7 +54,7 @@ if __name__ == '__main__':
     attrs = [a.strip() for a in attrs]
     print 'Attributes:', attrs
     setAttrVars(attrs)
-    
+
     with open(resourcesFilename) as f:
         resAttrsStr = f.readlines()
     resAttrsStr = [x.strip() for x in resAttrsStr]    
@@ -73,28 +75,28 @@ if __name__ == '__main__':
 
     with open(reqsFilename) as f:
         reqsStr = [x.strip() for x in f.readlines()]
-        
-    reqs = []
-    for reqStr in reqsStr:
-        if not reqStr.startswith('('):
-            print reqStr
-        else:
-            reqs.append(ctl_grammar.parseRequirement(reqStr))
-    
-    policy = None
 
-    '''    
-    if algorithm == 'decompose':
-        policy = decompose_synth.synth(graph, reqs)
-    elif algorithm == 'negative':
-        policy = negative_synth.synth(graph, reqs)
-    elif algorithm == 'boolean-policy':
-        policy = boolean_policy_synth.synth(graph, reqs, attrs)
+    if len(sys.argv) == 5:
+        policy = unsat
+        num_ors = 1
+        num_enum = 0
+        num_numeric = 0
+        while policy == unsat:
+            min_size = min(num_ors, num_enum, num_numeric)
+            inc = False
+            if num_ors == min_size:
+                num_ors += 1
+                inc = True
+            if not inc and num_enum == min_size:
+                num_enum += 1
+                inc = True
+            if not inc and num_numeric == min_size:
+                num_numeric += 1
+            print 'Template size', num_ors, num_enum, num_numeric
+            policy = solve_synthesis_instance(reqsStr, num_ors, num_enum, num_numeric)        
     else:
-    '''
-    policy = policy_synth.synth(graph, reqs, attrs)
-    #policy = decompose_synth.synth(graph, reqs)
-        
+        policy = solve_synthesis_instance(reqsStr, int(sys.argv[5]), int(sys.argv[6]), int(sys.argv[7]))
+                                       
     if policy == unsat:
         print 'No solution was found'
         sys.exit(-1)
