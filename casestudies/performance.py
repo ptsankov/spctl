6a852551c2dcfe3660e53a289d074ad1428796fb
Petar Tsankov <petar.tsankov@gmail.com>
2016-05-03 00:05:39 +0200

    performance code

--- casestudies/performance.py
+++ casestudies/performance.py
@@ -3,14 +3,15 @@ Created on May 2, 2016
 
 @author: ptsankov
 '''
-import sys
+from tabulate import tabulate
 import os
 import shutil
 from synthesizer import synth
-from synthesizer.utils import stats, measurements
+from synthesizer.utils import stats
+import statistics
 
 LOG_DIR = os.path.abspath('casestudies/logs')
-NUM_RUNS = 4
+NUM_RUNS = 10
 
 def measurePerformance():
     
@@ -26,8 +27,8 @@ def measurePerformance():
     os.mkdir(LOG_DIR)
     
     data = {}
-    
-    for case_study in ['Airport']:#configs.keys():
+    case_studies = list(configs.keys())
+    for case_study in case_studies:
         data[case_study] = {}
         data[case_study][stats.SYNTHESIS_TIME] = []
         for i in range(NUM_RUNS):
@@ -36,7 +37,18 @@ def measurePerformance():
             data[case_study][stats.NUMBER_OF_PEPS] = stats_map[stats.NUMBER_OF_PEPS]
             data[case_study][stats.NUMBER_OF_REQUIREMENTS] = stats_map[stats.NUMBER_OF_REQUIREMENTS]
             data[case_study][stats.NUMBER_OF_RESOURCES] = stats_map[stats.NUMBER_OF_RESOURCES]
-            
-            
-    print 'DATAAAA'
+            dst_log_file = os.path.join(LOG_DIR, '{}_run{}.log'.format(case_study, i))
+            shutil.copyfile(stats_map[stats.LOG_FILE], dst_log_file)
+    
     print data
+    
+    table = []
+    table.append(['Metric'] + configs.keys())
+    table.append(['Number of requirements'] + [data[case_study][stats.NUMBER_OF_REQUIREMENTS] for case_study in case_studies])
+    table.append(['Number of PEPs'] + [data[case_study][stats.NUMBER_OF_PEPS] for case_study in case_studies])
+    table.append(['Number of subspaces'] + [data[case_study][stats.NUMBER_OF_RESOURCES] for case_study in case_studies])
+    table.append(['Synthesis time (mean)'] + [statistics.mean(data[case_study][stats.SYNTHESIS_TIME]) for case_study in case_studies])
+    table.append(['Synthesis time (stdev)'] + [statistics.stdev(data[case_study][stats.SYNTHESIS_TIME]) for case_study in case_studies])
+    
+    
+    print tabulate(table, headers="firstrow")
